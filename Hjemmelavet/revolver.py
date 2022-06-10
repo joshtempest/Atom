3af8f6457c379b82ba253c17bde8dd3274fdd110
joshtempest <joshuabussink29@gmail.com>
2022-06-10 10:59:07 +0200

    h

---
+++ Hjemmelavet/revolver.py
@@ -0,0 +1,21 @@
+import keyboard
+import time
+shots = 0
+repeats = 1
+running = True
+
+while running == True:
+    if shots == 0 and repeats == 1:
+        print('out of ammo')
+        repeats = 0
+    if keyboard.is_pressed('esc'):
+        exit()
+    if keyboard.is_pressed("r"):
+        print('reloaded')
+        shots = 6
+        repeats = 1
+    while shots != 0:
+        if keyboard.is_pressed("space"):
+            print("bang!")
+            shots = shots - 1
+            time.sleep(0.3)
